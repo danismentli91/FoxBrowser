2d00df2d9b806f20e0f04bf57096036f8ae06345
danismentli91 <76398180+danismentli91@users.noreply.github.com>
2026-08-10 03:09:04 +0300

    Add settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral(); maven("https://maven.mozilla.org/maven2") } }
+rootProject.name = "FoxBrowser"
+include(":app")
